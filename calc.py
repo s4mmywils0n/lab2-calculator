ae6c65b31192a2d141a96ff54cd447877e440244
s4mmywils0n <143545509+s4mmywils0n@users.noreply.github.com>
2026-09-05 23:47:20 -0400

    feat: add substraction function. Resolves #1

--- calc.py
+++ calc.py
@@ -2,9 +2,25 @@ Welcome to Calculator
 
 def add(a,b): return a + b
 
+def  substract(a,b): return a - b
+
+
 num1 =  input("add first number")
 num2 = input("add second number ")
 
+def main()
+print("1. Addition")
+print("2. Substract")
+
+choice = input("select operation"
+if choice == "1":
 result = add(num1,num2)
+if choice == "2"
+result = substract(num1,num2)
 
 print("the results are : "+ result )
+
+
+
+
+
